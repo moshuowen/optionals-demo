1434b39b072a4660979dd9c8b41e9b1c5f74a0a2
moshuowen <2083643622@qq.com>
2022-10-26 08:57:52 +0800

    changes

--- src/main/java/cn/msw/functional/Main.java
+++ src/main/java/cn/msw/functional/Main.java
@@ -10,10 +10,14 @@ public class Main {
 
     }
 
-    public static void define(){
+    public static void define() {
 
-        Function<String, Integer> function1 = String::length;
+        Function<String, Integer> function1 = (String s) -> s.length();
         System.out.println(function1.apply("hello world"));
 
+        Function<String, Integer> function2 = (String s) -> {
+            return s.length();
+        };
+        System.out.println(function1.apply("hello world"));
     }
 }

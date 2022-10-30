f981472ca2e6f1848362f2aed106f2b8f638e002
moshuowen <2083643622@qq.com>
2022-10-30 23:51:32 +0800

    optional

--- src/main/java/cn/msw/rebase/Main.java
+++ src/main/java/cn/msw/rebase/Main.java
@@ -1,8 +1,14 @@
 package cn.msw.rebase;
 
+import java.util.Optional;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        String name=  null;
+        Optional.ofNullable(name).orElseThrow(() -> new IllegalArgumentException("name is null"));
+        System.out.println(name);
     }
 
 }

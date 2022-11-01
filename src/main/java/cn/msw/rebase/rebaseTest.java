b0cdab88bc2704ad78fc476e2517a4762b1c98cc
moshuowen <2083643622@qq.com>
2022-11-01 08:46:38 +0800

    changes

--- src/main/java/cn/msw/rebase/rebaseTest.java
+++ src/main/java/cn/msw/rebase/rebaseTest.java
@@ -3,6 +3,10 @@ package cn.msw.rebase;
 public class rebaseTest {
 
     public static void main(String[] args) {
-        System.out.println("rebase..");
+            String s1 = new String("字符串");
+            String s2 = "字符串";
+            System.out.println(s2 == s2.intern());
+            System.out.println(s1 == s1.intern());
+            System.out.println(s1.intern() == s2.intern());
     }
 }

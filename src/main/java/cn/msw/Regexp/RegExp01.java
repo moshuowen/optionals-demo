cc24f1f679a1b456cb695f3cd7802772b6281d39
moshuowen <2083643622@qq.com>
2022-11-06 14:53:40 +0800

    regex

--- src/main/java/cn/msw/Regexp/RegExp01.java
+++ src/main/java/cn/msw/Regexp/RegExp01.java
@@ -10,10 +10,12 @@ public class RegExp01
 
     public static void main(String[] args) {
 
-        String content = "abcABC";
+        String content = "a1bcA1B2C";
+        // [^a-zA-Z] 匹配除了a-z和A-Z之外的所有字符
+        Pattern compile = compile("[^a-zA-Z]");
 //        Pattern compile = compile("(?i)abc");
         // (?i)abc  (?i)表示忽略大小写, a((?i)b)c 表示abc aBc都可以匹配,CASE_INSENSITIVE 也可以表示忽略大小写
-        Pattern compile = compile("a((?i)b)c", CASE_INSENSITIVE);
+//        Pattern compile = compile("a((?i)b)c", CASE_INSENSITIVE);
 //        Pattern compile = compile("a(?i)bc");
         Matcher matcher = compile.matcher(content);
         while (matcher.find()) {
